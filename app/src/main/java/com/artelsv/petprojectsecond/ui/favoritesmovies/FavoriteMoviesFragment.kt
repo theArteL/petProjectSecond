9f858df83a93fded56e35811e36880cac1f51ceb
sv <feo-97@mail.ru>
2021-10-15 19:38:55 +0200

    start of favorites

---
+++ app/src/main/java/com/artelsv/petprojectsecond/ui/favoritesmovies/FavoriteMoviesFragment.kt
@@ -0,0 +1,33 @@
+package com.artelsv.petprojectsecond.ui.favoritesmovies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.artelsv.petprojectsecond.databinding.FragmentFavoriteMoviesBinding
+import com.github.terrakok.cicerone.Router
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
+
+class FavoriteMoviesFragment : DaggerFragment() {
+
+    @Inject
+    private lateinit var viewModel: FavoriteMoviesViewModel
+
+    @Inject
+    lateinit var router: Router
+
+    private val binding: FragmentFavoriteMoviesBinding by viewBinding(createMethod = CreateMethod.INFLATE)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = viewModel
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/artelsv/petprojectsecond/ui/favoritesmovies/FavoriteMoviesViewModel.kt
@@ -0,0 +1,6 @@
+package com.artelsv.petprojectsecond.ui.favoritesmovies
+
+import com.artelsv.petprojectsecond.ui.base.BaseViewModel
+
+class FavoriteMoviesViewModel : BaseViewModel() {
+}
